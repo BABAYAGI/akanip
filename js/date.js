3abedc820cf12db47cc286d345e7af86689cc197
babayagi <darsan_hirani@hotmail.com>
2019-08-19 07:57:30 +0300

    male figured out on java

--- js/date.js
+++ js/date.js
@@ -32,6 +32,17 @@ months[10] = "October"
 months[11] = "November"
 months[12] = "December"
 
+//Akan names Array
+maleakannames = new MakeArray(7);
+akannames[0]= "kwame"
+akannames[1]= "kwasi"
+akannames[2]= "kwadwo"
+akannames[3]= "kwabena"
+akannames[4]= "kwaki"
+akannames[5]= "yaw"
+akannames[6]= "kwame"
+
+
 //Day of Week Function
 function compute(form) {
    var val1 = parseInt(form.day.value, 10)
@@ -62,9 +73,42 @@ function compute(form) {
    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
    var val9 = parseInt(val8/7, 10)
    var val0 = val8-(val9*7)
-   form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+   form.result1.value = maleakannames[val0]
    form.result2.value = days[val0]
 }   
 
+//Day of Week Function
+function compute(form) {
+  var val1 = parseInt(form.day.value, 10)
+  if ((val1 < 0) || (val1 > 31)) {
+     alert("Day is out of range")
+  }
+  var val2 = parseInt(form.month.value, 10)
+  if ((val2 < 0) || (val2 > 12)) {
+     alert("Month is out of range")
+  }  
+  var val2x = parseInt(form.month.value, 10)
+  var val3 = parseInt(form.year.value, 10)
+  if (val3 < 1900) {
+     alert("You're that old!")
+  }
+  if (val2 == 1) {
+     val2x = 13;
+     val3 = val3-1
+  }
+  if (val2 == 2) {
+     val2x = 14;
+     val3 = val3-1
+  }
+  var val4 = parseInt(((val2x+1)*3)/5, 10)
+  var val5 = parseInt(val3/4, 10)
+  var val6 = parseInt(val3/100, 10)
+  var val7 = parseInt(val3/400, 10)
+  var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+  var val9 = parseInt(val8/7, 10)
+  var val0 = val8-(val9*7)
+  form.result1.value = akannames[val0]
+  form.result2.value = days[val0]
+}   
 
 
